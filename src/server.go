73560ea01aba3ccbd0d182cdead8a3727c56bc09
Matt Maloney <matt.maloney@softrams.com>
2022-06-03 14:20:36 -0400

    running server

--- src/server.go
+++ src/server.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
-	// "log"
-	// "net/http"
+	"log"
+	"net/http"
 )
 
 func main() {
 	fmt.Printf("Starting server on port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
