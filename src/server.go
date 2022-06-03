17a7b48d23729a2906400beffbaf85c20552f1d6
Matt Maloney <matt.maloney@softrams.com>
2022-06-03 14:41:59 -0400

    adding request handler

--- src/server.go
+++ src/server.go
@@ -7,6 +7,13 @@ import (
 )
 
 func main() {
+	//
+	// requwst handler
+	//
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello Request Handler")
+	})
+
 	fmt.Printf("Starting server on port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
