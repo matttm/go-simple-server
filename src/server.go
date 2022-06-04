78e324a2e1254683484b4103b1653832cf642a9f
Matt Maloney <matt.maloney@softrams.com>
2022-06-04 11:51:48 -0400

    adding form submission

--- src/server.go
+++ src/server.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func helloRequestHandler(w http.ResponseWriter, r *http.Request) {
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "HTTP 404: File Not Found", http.StatusNotFound)
 		return
@@ -18,13 +18,27 @@ func helloRequestHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello Request Handler")
 }
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Form submitted\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "name %s\n", name)
+	fmt.Fprintf(w, "address %s\n", address)
+}
+
 func main() {
-	fileServer := http.FileServer(http.Dir("/public"))
+	fileServer := http.FileServer(http.Dir("./public"))
 	//
 	// requwst handler
 	//
 	http.Handle("/", fileServer)
-	http.HandleFunc("/hello", helloRequestHandler)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server on port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
