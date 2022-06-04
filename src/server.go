3f1df4dbc3bbf6ca6e46e9029e5e3823b32bb24a
Matt Maloney <matt.maloney@softrams.com>
2022-06-04 08:55:42 -0400

    adding basic security

--- src/server.go
+++ src/server.go
@@ -6,13 +6,23 @@ import (
 	"net/http"
 )
 
+func helloRequestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "HTTP 404: File Not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "HTTP Method not supported", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "Hello Request Handler")
+}
+
 func main() {
 	//
 	// requwst handler
 	//
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Request Handler")
-	})
+	http.HandleFunc("/hello", helloRequestHandler)
 
 	fmt.Printf("Starting server on port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
